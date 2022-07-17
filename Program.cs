5913efd48a4d9eca5787f38fcdf17c275f89488d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-07-17 17:55:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Modified_Bubble_Sort
+{
+    class Program
+    {
+        const int ELEMENTS = 20;
+        static void Main(string[] args)
+        {
+            int i;
+            Random rnd = new Random();
+
+            int[] arr = new int[ELEMENTS];
+            for(i=0; i<=ELEMENTS-1; i++)
+            {
+                arr[i] = rnd.Next(100);
+            }
+
+            ModifiedBubbleSort(arr);
+            for(i=0; i<=ELEMENTS-1; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+
+        static void ModifiedBubbleSort(int[] arr)
+        {
+            int i, j, temp;
+            bool flag;
+            for(i = 1; i <= arr.Length - 1; i++)
+            {
+                flag = false;
+                for(j = arr.Length - 1; j >= i; j--)
+                {
+                    if(arr[j] < arr[j-1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+
+                        flag = true;
+                    }
+                }
+
+                if(flag == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
